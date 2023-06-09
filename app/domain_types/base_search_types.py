6d603bfaa31666b02be6e99ffa8620f22b015df2
Kiran Kharade <Kiran.Kharade@inflectionzone.com>
2023-06-09 11:45:24 +0530

    Updated domain models

--- app/domain_types/base_search_types.py
+++ app/domain_types/base_search_types.py
@@ -1,20 +1,19 @@
+from typing import Optional
 from pydantic import BaseModel, Field
 from datetime import datetime
 
 class BaseSearchFilter(BaseModel):
-    CreatedDateFrom: datetime = Field(Optional=True)
-    CreatedDateTo: datetime = Field(Optional=True)
-    OrderBy: str = Field(Optional=True)
-    OrderByDescending: bool = Field(Optional=True)
-    PageIndex: int = Field(Optional=True)
-    ItemsPerPage: int = Field(Optional=True)
+    CreatedDateFrom  : Optional[datetime] = Field(default=None)
+    CreatedDateTo    : Optional[datetime] = Field(default=None)
+    OrderBy          : Optional[str]      = Field(default='CreatedAt', min_length=2, max_length=64)
+    OrderByDescending: Optional[bool]     = Field(default=True)
+    PageIndex        : Optional[int]      = Field(default=0, ge=0, le=10000)
+    ItemsPerPage     : Optional[int]      = Field(default=10, ge=1, le=100)
 
 class BaseSearchResults(BaseModel):
-    TotalCount: int = Field(default=0, Optional=False)
-    RetrievedCount: int = Field(default=0, Optional=False)
-    PageIndex: int = Field(default=0, Optional=False)
-    ItemsPerPage: int = Field(default=0, Optional=False)
-    OrderBy: str = Field(Optional=True)
-    OrderByDescending: bool = Field(Optional=True)
-    Items: list = Field(default=None, Optional=True)
-
+    TotalCount       : int  = Field(default=0, ge=0)
+    RetrievedCount   : int  = Field(default=0, ge=0)
+    PageIndex        : int  = Field(default=0, ge=0, le=10000)
+    ItemsPerPage     : int  = Field(default=10, ge=1, le=100)
+    OrderBy          : str  = Field(default='CreatedAt', min_length=2, max_length=64)
+    OrderByDescending: bool = Field(Default=True)
--- app/domain_types/order_domain_models.py
+++ app/domain_types/order_domain_models.py
@@ -1,17 +1,16 @@
-from typing import Optional, List
+from typing import List, Optional
 from pydantic import UUID4, BaseModel, Field
-from datetime import datetime
-
+from app.domain_types.order_status_types import OrderStatusTypes
 from app.domain_types.base_search_types import BaseSearchFilter, BaseSearchResults
 
 class OrderCreateModel(BaseModel):
-    OrderTypeId: UUID4 = Field(default=None, Optional=True)
-    CustomerId: UUID4 = Field(default=None, Optional=False)
-    CartId: UUID4 = Field(default=None, Optional=True)
-    TipApplicable: bool = Field(default=False, Optional=False)
-    Notes: str = Field(default=None, Optional=True)
-    OrderLineItems: list = Field(default=[], Optional=True)
-    CouponId: UUID4 = Field(default=None, Optional=True)
+      OrderTypeId    : Optional[UUID4 | None] = Field(default=None)
+      CustomerId     : UUID4                  = Field(...)
+      CartId         : Optional[UUID4 | None] = Field(default=None)
+      TipApplicable  : Optional[bool | None]  = Field(default=False)
+      Notes          : Optional[str | None]   = Field(default=None, min_length=5, max_length=1024)
+      OrderLineItems : Optional[list]         = Field(default=[])
+      CouponId       : Optional[UUID4 | None] = Field(default=None)
 
 # Please note that -
 # CustomerId, OrderLineItems, OrderStatus cannot be updated through regular update API.
@@ -20,57 +19,57 @@ class OrderCreateModel(BaseModel):
 # OrderLineItems are updated through a different set of APIs.
 
 class OrderUpdateModel(BaseModel):
-    OrderTypeId: UUID4 = Field(default=None, Optional=True)
-    CartId: UUID4 = Field(default=None, Optional=True)
-    OrderDiscount: float = Field(Optional=True)
-    TipApplicable: bool = Field(Optional=True)
-    TipAmount: float = Field(Optional=True)
-    Notes: str = Field(default=None, Optional=True)
-    CouponId: UUID4 = Field(default=None, Optional=True)
-    PaymentTransactionId: UUID4 = Field(default=None, Optional=True)
-    RefundTransactionId: UUID4 = Field(default=None, Optional=True)
+    OrderTypeId         : Optional[UUID4]
+    CartId              : Optional[UUID4]
+    OrderDiscount       : Optional[float] = Field(ge=0.0)
+    TipApplicable       : Optional[bool]
+    TipAmount           : Optional[float] = Field(ge=0.0)
+    Notes               : Optional[str]   = Field(min_length=5, max_length=1024)
+    CouponId            : Optional[UUID4]
+    PaymentTransactionId: Optional[UUID4]
+    RefundTransactionId : Optional[UUID4]
 
 class OrderSearchFilter(BaseSearchFilter):
-    CustomerId: UUID4 = Field(Optional=True)
-    CartId: UUID4 = Field(Optional=True)
-    TotalItemsCountGreaterThan: int = Field(Optional=True)
-    TotalItemsCountLessThan: int = Field(Optional=True)
-    OrderDiscountGreaterThan: float = Field(Optional=True)
-    OrderDiscountLessThan: float = Field(Optional=True)
-    TipApplicable: bool = Field(Optional=True)
-    TotalAmountGreaterThan: float = Field(Optional=True)
-    TotalAmountLessThan: float = Field(Optional=True)
-    OrderLineItemId: UUID4 = Field(Optional=True)
-    CouponId: UUID4 = Field(Optional=True)
-    OrderStatus: str = Field(Optional=True)
-    OrderType: str = Field(Optional=True)
-    CreatedBefore: datetime = Field(Optional=True)
-    CreatedAfter: datetime = Field(Optional=True)
-    PastMonths: int = Field(Optional=True)
+    CustomerId                : Optional[UUID4]
+    CartId                    : Optional[UUID4]
+    TotalItemsCountGreaterThan: Optional[int]      = Field(ge=0, le=100)
+    TotalItemsCountLessThan   : Optional[int]      = Field(ge=1, le=100)
+    OrderDiscountGreaterThan  : Optional[float]    = Field(ge=0.0)
+    OrderDiscountLessThan     : Optional[float]    = Field(ge=0.0)
+    TipApplicable             : Optional[bool]
+    TotalAmountGreaterThan    : Optional[float]    = Field(ge=0.0)
+    TotalAmountLessThan       : Optional[float]    = Field(ge=0.0)
+    OrderLineItemId           : Optional[UUID4]
+    CouponId                  : Optional[UUID4]
+    OrderStatus               : Optional[OrderStatusTypes]
+    OrderType                 : Optional[str]      = Field(min_length=2, max_length=64)
+    CreatedBefore             : Optional[datetime]
+    CreatedAfter              : Optional[datetime]
+    PastMonths                : Optional[int]      = Field(ge=0, le=12)
 
 class OrderResponseModel(BaseModel):
-    id: UUID4
-    DisplayCode: str
-    InvoiceNumber: str
-    CartId: UUID4
-    TotalItemsCount: int
-    OrderDiscount: float
-    TipApplicable: bool
-    TipAmount: float
-    TotalTax: float
-    TotalDiscount: float
-    TotalAmount: float
-    Notes: str
-    OrderLineItems: List[dict]
-    CouponId: UUID4
-    Coupon: dict
-    PaymentTransaction: dict
+    id                 : UUID4
+    DisplayCode        : str
+    InvoiceNumber      : str
+    CartId             : UUID4
+    TotalItemsCount    : int
+    OrderDiscount      : float
+    TipApplicable      : bool
+    TipAmount          : float
+    TotalTax           : float
+    TotalDiscount      : float
+    TotalAmount        : float
+    Notes              : str
+    OrderLineItems     : List[dict]
+    CouponId           : UUID4
+    Coupon             : dict
+    PaymentTransaction : dict
     RefundTransactionId: dict
-    OrderStatus: str
-    OrderType: str
-    CreatedAt: datetime
-    UpdatedAt: datetime
+    OrderStatus        : str
+    OrderType          : str
+    CreatedAt          : datetime
+    UpdatedAt          : datetime
 
 
 class OrderSearchResults(BaseSearchResults):
-    Items: List[OrderResponseModel] = Field(default=None, Optional=True)
+    Items: List[OrderResponseModel] = Field(default=[])
