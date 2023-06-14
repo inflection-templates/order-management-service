bbca602bcc6f5364661be75207c8f0e4079fb82e
Kiran Kharade <Kiran.Kharade@inflectionzone.com>
2023-06-14 18:41:57 +0530

    Minor updates to customer routes

--- README.md
+++ README.md
@@ -12,13 +12,18 @@ Order management service is a generic multi-purpose micro-service.
 2. Add missing routes
    1. address
    2. remaining customer routes
+      1. create,
+      2. get_by_id,
+      3. update,
+      4. search,
+      5. delete
    3. remaining order routes
    4. cart
    5. coupon
    6. merchant
    7. order history
    8. order line items
-   9. order type
+   9.  order type
    10. payment transactions
 3. Missing integration tests for all the routes
 4. Update the documentation
--- app/api/customer.py
+++ app/api/customer.py
@@ -13,23 +13,37 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-@router.post("/", status_code=201, response_model=CustomerResponseModel|None)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=CustomerResponseModel|None)
 async def create_customer(model: CustomerCreateModel, db_session = Depends(get_db_session)):
-    print_colorized_json(model)
-    customer = customer_service.create_customer(db_session, model)
-    return customer
+    try:
+        print_colorized_json(model)
+        customer = customer_service.create_customer(db_session, model)
+        return customer
+    except Exception as e:
+        print(e)
+        db_session.rollback()
+        raise e
+    finally:
+        db_session.close()
 
 @router.get("/{id}", status_code=status.HTTP_302_FOUND, response_model=CustomerResponseModel|None)
 async def get_customer_by_id(id: str, db_session = Depends(get_db_session)):
-    customer_id = validate_uuid4(id)
-    return customer_service.get_customer_by_id(db_session, customer_id)
+    try:
+        customer_id = validate_uuid4(id)
+        return customer_service.get_customer_by_id(db_session, customer_id)
+    except Exception as e:
+        print(e)
+        db_session.rollback()
+        raise e
+    finally:
+        db_session.close()
 
 @router.put("/{id}", status_code=200, response_model=CustomerResponseModel|None)
 async def update_customer(id: str, model: CustomerUpdateModel, db_session = Depends(get_db_session)):
     customer_id = validate_uuid4(id)
-    return customer_service.update_customer(db_session, customer_id, model) 
+    return customer_service.update_customer(db_session, customer_id, model)
 
 # @router.get("/search", status_code=status.HTTP_302_FOUND, response_model=CustomerSearchResults|None)
 # async def search_customer(filter: str = Query(None), db_session = Depends(get_db_session)):
-    
+
 #     return customer_service.search_customer(db_session, filter)
