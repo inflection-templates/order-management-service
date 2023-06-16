59cc582723316d48fc2c06b23fa45e47d4d953d3
Priyanka-Inflectionzone <priyanka.kale@inflectionzone.com>
2023-06-16 12:43:31 +0530

    added route and service for delete customer

--- app/api/customer.py
+++ app/api/customer.py
@@ -1,10 +1,10 @@
 import uuid
-from fastapi import APIRouter, Depends, status, Query, Body
+from fastapi import APIRouter, Depends, status
 from app.common.utils import print_colorized_json, validate_uuid4
 from app.database.database_accessor import get_db_session
 from app.database.services import customer_service
 from app.domain_types.schemas.customer import CustomerCreateModel, CustomerUpdateModel, CustomerResponseModel, CustomerSearchFilter, CustomerSearchResults
-from app.domain_types.schemas.order import OrderCreateModel, OrderResponseModel
+from app.domain_types.miscellaneous.response_model import ResponseModel, ResponseStatusTypes
 
 router = APIRouter(
     prefix="/customers",
@@ -13,23 +13,68 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-@router.post("/", status_code=201, response_model=CustomerResponseModel|None)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=ResponseModel[CustomerResponseModel]|None)
 async def create_customer(model: CustomerCreateModel, db_session = Depends(get_db_session)):
-    print_colorized_json(model)
-    customer = customer_service.create_customer(db_session, model)
-    return customer
+    try:
+        print_colorized_json(model)
+        customer = customer_service.create_customer(db_session, model)
+        message = "Customer created successfully"
+        resp = ResponseModel[CustomerResponseModel](Message=message, Data=customer)
+        # logger.info(resp)
+        return resp
+    except Exception as e:
+        print(e)
+        db_session.rollback()
+        raise e
+    finally:
+        db_session.close()
 
-@router.get("/{id}", status_code=status.HTTP_302_FOUND, response_model=CustomerResponseModel|None)
+
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=ResponseModel[CustomerResponseModel]|None)
 async def get_customer_by_id(id: str, db_session = Depends(get_db_session)):
-    customer_id = validate_uuid4(id)
-    return customer_service.get_customer_by_id(db_session, customer_id)
+    try:
+        customer_id = validate_uuid4(id)
+        customer = customer_service.get_customer_by_id(db_session, customer_id)
+        message = "Customer found successfully"
+        resp = ResponseModel[CustomerResponseModel](Message=message, Data=customer)
+        # logger.info(resp)
+        return resp
+    except Exception as e:
+            print(e)
+            db_session.rollback()
+            raise e
+    finally:
+            db_session.close()
 
-@router.put("/{id}", status_code=200, response_model=CustomerResponseModel|None)
+@router.put("/{id}", status_code=200, response_model=ResponseModel[CustomerResponseModel]|None)
 async def update_customer(id: str, model: CustomerUpdateModel, db_session = Depends(get_db_session)):
-    customer_id = validate_uuid4(id)
-    return customer_service.update_customer(db_session, customer_id, model) 
+    try:
+        customer_id = validate_uuid4(id)
+        customer = customer_service.update_customer(db_session, customer_id, model) 
+        message = "Customer updated successfully"
+        resp = ResponseModel[CustomerResponseModel](Message=message, Data=customer)
+        # logger.info(resp)
+        return resp
+    except Exception as e:
+            print(e)
+            db_session.rollback()
+            raise e
+    finally:
+            db_session.close()
 
-# @router.get("/search", status_code=status.HTTP_302_FOUND, response_model=CustomerSearchResults|None)
-# async def search_customer(filter: str = Query(None), db_session = Depends(get_db_session)):
-    
-#     return customer_service.search_customer(db_session, filter)
+@router.delete("/{id}", status_code=200, response_model=ResponseModel[CustomerResponseModel]|None)
+async def delete_customer(id: str, db_session = Depends(get_db_session)):
+    try:
+        customer_id = validate_uuid4(id)
+        customer = customer_service.delete_customer(db_session, customer_id) 
+        message = "Customer deleted successfully"
+        resp = ResponseModel[CustomerResponseModel](Message=message, Data=customer)
+        # logger.info(resp)
+        return resp
+    except Exception as e:
+            print(e)
+            db_session.rollback()
+            raise e
+    finally:
+            db_session.close()
+
--- app/database/services/customer_service.py
+++ app/database/services/customer_service.py
@@ -17,21 +17,21 @@ def create_customer(session: Session, model: CustomerCreateModel) -> CustomerRes
             func.lower(Customer.Email) == func.lower(model.Email)
         ).first()
         if existing_customer:
-            raise Conflict("Customer with email {model.Email} already exists!")
+            raise Conflict(f"Customer with email {model.Email} already exists!")
 
     if model.Phone != None and model.Phone != "":
         existing_customer = session.query(Customer).filter(
             Customer.Phone == model.Phone
         ).first()
         if existing_customer:
-            raise Conflict("Customer with phone {model.Phone} already exists!")
+            raise Conflict(f"Customer with phone {model.Phone} already exists!")
 
     if model.TaxNumber != None and model.TaxNumber != "":
         existing_customer = session.query(Customer).filter(
             func.lower(Customer.TaxNumber) == func.lower(model.TaxNumber)
         ).first()
         if existing_customer:
-            raise Conflict("Customer with tax number {model.TaxNumber} already exists!")
+            raise Conflict(f"Customer with tax number {model.TaxNumber} already exists!")
 
     model_dict = model.dict()
     db_model = Customer(**model_dict)
@@ -46,84 +46,37 @@ def create_customer(session: Session, model: CustomerCreateModel) -> CustomerRes
     return customer.__dict__
 
 def get_customer_by_id(session: Session, customer_id: str) -> CustomerResponseModel:
-    try:
-        customer = session.query(Customer).filter(Customer.id == customer_id).first()
-        if not customer:
-          raise HTTPException(status_code=404, detail=f"Customer with id {customer_id} not found")
-    except Exception as e:
-        print(e)
-        session.rollback()
-        raise e
-    finally:
-        session.close()
+    customer = session.query(Customer).filter(Customer.id == customer_id).first()
+    if not customer:
+        raise HTTPException(status_code=404, detail=f"Customer with id {customer_id} not found")
 
     # customer = CustomerResponseModel(**Customer.dict(), id=uuid.uuid4(), DisplayCode="1234", InvoiceNumber="1234")
     print_colorized_json(customer)
     return customer.__dict__
 
 def update_customer(session: Session, customer_id: str, model: CustomerUpdateModel) -> CustomerResponseModel:
-    try:
-        # update_data = {}
-        # if model.Name:
-        #     update_data["Name"] = model.Name
-        # if model.Email :
-        #     update_data["Email"] = model.Email
-        # if model.PhoneCode:
-        #     update_data["PhoneCode"] = model.PhoneCode
-        # if model.Phone:
-        #     update_data["Phone"] = model.Phone
-        # if model.TaxNumber :
-        #     update_data["TaxNumber"] = model.TaxNumber
-        # if model.ProfilePicture:
-        #     update_data["ProfilePicture"] = model.ProfilePicture
-        customer = session.query(Customer).filter(Customer.id == customer_id).first()
-        if not customer:
-<<<<<<< HEAD
-          raise HTTPException(status_code=404, detail=f"Customer with id {customer_id} not found")
+    customer = session.query(Customer).filter(Customer.id == customer_id).first()
+    if not customer:
+        raise HTTPException(status_code=404, detail=f"Customer with id {customer_id} not found")
         
-=======
-          raise HTTPException(status_code=404, detail="Customer with id {customer_id} not found")
+    update_data = model.dict(exclude_unset=True)
+    update_data["UpdatedAt"] = dt.datetime.now()
+    session.query(Customer).filter(Customer.id == customer_id).update(update_data, synchronize_session="auto")
 
->>>>>>> origin/main
-        update_data = model.dict(exclude_unset=True)
-        update_data["UpdatedAt"] = dt.datetime.now()
-        session.query(Customer).filter(Customer.id == customer_id).update(update_data, synchronize_session="auto")
-
-        session.commit()
-        session.refresh(customer)
-    except Exception as e:
-        print(e)
-        session.rollback()
-        raise e
-    finally:
-        session.close()
-
-    # customer = CustomerResponseModel(**Customer.dict(), id=uuid.uuid4(), DisplayCode="1234", InvoiceNumber="1234")
+    session.commit()
+    session.refresh(customer)
+    
     print_colorized_json(customer)
     return customer.__dict__
 
-# def search_customer(session: Session, filter: str = Query(None)) -> CustomerSearchResults:
-#     try:
-#         query = session.query(Customer)
-#         if filter.Name:
-#           query = query.filter(Customer.Name == filter.Name)
-#         if filter.Email:
-#           query = query.filter(Customer.Email == filter.Email)
-#         if filter.PhoneCode:
-#           query = query.filter(Customer.PhoneCode == filter.PhoneCode)
-#         if filter.Phone:
-#           query = query.filter(Customer.Phone == filter.Phone)
-#         if filter.TaxNumber:
-#           query = query.filter(Customer.TaxNumber == filter.TaxNumber)
-#         customers = query.all()
+def delete_customer(session: Session, customer_id: str) -> CustomerResponseModel:
+    customer = session.query(Customer).get(customer_id)
+    if not customer:
+        raise HTTPException(status_code=404, detail=f"Customer with id {customer_id} not found")
+        
+    session.delete(customer)
 
-#     except Exception as e:
-#         print(e)
-#         session.rollback()
-#         raise e
-#     finally:
-#         session.close()
-
-#     # customer = CustomerResponseModel(**Customer.dict(), id=uuid.uuid4(), DisplayCode="1234", InvoiceNumber="1234")
-#     print_colorized_json(customers)
-#     return customers.__dict__
+    session.commit()
+       
+    print_colorized_json(customer)
+    return customer.__dict__
