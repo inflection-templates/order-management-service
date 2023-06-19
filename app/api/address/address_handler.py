3c9b0c99fb9882432a3d461e40efb3ef7f24bdfe
Priyanka-Inflectionzone <priyanka.kale@inflectionzone.com>
2023-06-19 17:58:26 +0530

    Added address handler and routes

---
+++ app/api/address/address_handler.py
@@ -0,0 +1,93 @@
+from app.common.utils import validate_uuid4
+from app.database.services import address_service
+from app.domain_types.miscellaneous.response_model import ResponseModel
+from app.domain_types.schemas.address import AddressResponseModel
+from app.telemetry.tracing import trace_span
+
+
+@trace_span("handler: create_address")
+def create_address_(model, db_session):
+    try:
+        address = address_service.create_address(db_session, model)
+        message = "Address created successfully"
+        resp = ResponseModel[AddressResponseModel](
+            Message=message, Data=address)
+        # logger.info(resp)
+        return resp
+    except Exception as e:
+        print(e)
+        db_session.rollback()
+        raise e
+    finally:
+        db_session.close()
+
+
+@trace_span("handler: get_address_by_id")
+def get_address_by_id_(id, db_session):
+    try:
+        address_id = validate_uuid4(id)
+        address = address_service.get_address_by_id(db_session, address_id)
+        message = "Address retrieved successfully"
+        resp = ResponseModel[AddressResponseModel](
+            Message=message, Data=address)
+        # logger.info(resp)
+        return resp
+    except Exception as e:
+        print(e)
+        db_session.rollback()
+        raise e
+    finally:
+        db_session.close()
+
+
+@trace_span("handler: update_address")
+def update_address_(id, model, db_session):
+    try:
+        address_id = validate_uuid4(id)
+        address = address_service.update_address(db_session, address_id, model)
+        message = "Address updated successfully"
+        resp = ResponseModel[AddressResponseModel](
+            Message=message, Data=address)
+        # logger.info(resp)
+        return resp
+    except Exception as e:
+        print(e)
+        db_session.rollback()
+        raise e
+    finally:
+        db_session.close()
+
+
+@trace_span("handler: delete_address")
+def delete_customer_(id, db_session):
+    try:
+        customer_id = validate_uuid4(id)
+        customer = address_service.delete_address(db_session, customer_id)
+        message = "Address deleted successfully"
+        resp = ResponseModel[AddressResponseModel](
+            Message=message, Data=customer)
+        # logger.info(resp)
+        return resp
+    except Exception as e:
+        print(e)
+        db_session.rollback()
+        raise e
+    finally:
+        db_session.close()
+
+
+@trace_span("handler: search_addresses")
+def search_customers_(filter, db_session):
+    try:
+        addresses = address_service.search_customers(db_session, filter)
+        message = "Addresses retrieved successfully"
+        resp = ResponseModel[AddressResponseModel](
+            Message=message, Data=addresses)
+        # print_colorized_json(model)
+        return resp
+    except Exception as e:
+        db_session.rollback()
+        db_session.close()
+        raise e
+    finally:
+        db_session.close()
---
+++ app/api/address/address_routes.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, Depends, status, Query, Body
+from app.common.utils import print_colorized_json, validate_uuid4
+from app.database.database_accessor import get_db_session
+from app.api.address.address_handler import (
+    create_address_,
+    get_address_by_id_,
+    update_address_,
+    delete_address_,
+    search_addresses_
+)
+from app.domain_types.schemas.address import AddressCreateModel, AddressUpdateModel, AddressResponseModel, AddressSearchResults, AddressSearchFilter
+from app.domain_types.miscellaneous.response_model import ResponseModel, ResponseStatusTypes
+
+
+router = APIRouter(
+    prefix="/addresses",
+    tags=["addresses"],
+    dependencies=[],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.post("/", status_code=201, response_model=ResponseModel[AddressResponseModel] | None)
+async def create_address(model: AddressCreateModel, db_session=Depends(get_db_session)):
+    return create_address_(model, db_session)
+
+
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=ResponseModel[AddressResponseModel] | None)
+async def get_address_by_id(id: str, db_session=Depends(get_db_session)):
+    return get_address_by_id_(id, db_session)
+
+
+@router.put("/{id}", status_code=status.HTTP_200_OK, response_model=AddressResponseModel | None)
+async def update_address(id: str, model: AddressUpdateModel, db_session=Depends(get_db_session)):
+    return update_address_(id, model, db_session)
+
+
+@router.delete("/{id}", status_code=200, response_model=ResponseModel[AddressResponseModel] | None)
+async def delete_address(id: str, db_session=Depends(get_db_session)):
+    return delete_address(id, db_session)
+
+
+@router.get("/search", status_code=status.HTTP_200_OK, response_model=ResponseModel[AddressSearchResults | None])
+async def search_customer(
+        query_params: AddressSearchFilter = Depends(),
+        db_session=Depends(get_db_session)):
+    filter = AddressSearchFilter(**query_params.dict())
+    return search_addresses_(filter, db_session)
