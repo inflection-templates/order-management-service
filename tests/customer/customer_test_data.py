05381991686b539d2e845c5abe846a623cd9252a
Priyanka-Inflectionzone <priyanka.kale@inflectionzone.com>
2023-06-19 11:13:06 +0530

    modified customer_test_data for phone number

--- tests/customer/customer_test_data.py
+++ tests/customer/customer_test_data.py
@@ -4,12 +4,14 @@ import uuid
 
 from app.domain_types.schemas.customer import CustomerCreateModel
 
+
 def get_customer_create_model():
     fake = Faker()
     model = {
         "ReferenceId": uuid.uuid4(),
         "Name": fake.name(),
         "Email": fake.email(),
-        "Phone": fake.phoneNumber(),
+        "Phone": fake.phone_number().replace("-", ""),
     }
+    print(model)
     return model
