eaa4af8b5b6bc9b5edf067ae90087f028b489b6a
Priyanka-Inflectionzone <priyanka.kale@inflectionzone.com>
2023-06-30 12:55:07 +0530

    Added payment_transaction.service

---
+++ app/database/services/payment_transaction_service.py
@@ -0,0 +1,70 @@
+import datetime as dt
+import uuid
+from app.common.utils import print_colorized_json
+from app.database.models.payment_transaction import PaymentTransaction
+from app.domain_types.miscellaneous.exceptions import Conflict, NotFound
+from app.domain_types.schemas.payment_transaction import PaymentTransactionCreateModel, PaymentTransactionResponseModel, PaymentTransactionSearchFilter, PaymentTransactionSearchResults
+from sqlalchemy.orm import Session
+from sqlalchemy import func
+from app.telemetry.tracing import trace_span
+
+@trace_span("service: create_payment_transaction")
+def create_payment_transaction(session: Session, model: PaymentTransactionCreateModel) -> PaymentTransactionResponseModel:
+    model_dict = model.dict()
+    db_model = PaymentTransaction(**model_dict)
+    db_model.UpdatedAt = dt.datetime.now()
+    session.add(db_model)
+    session.commit()
+    temp = session.refresh(db_model)
+    payment_transaction = db_model
+
+    return payment_transaction.__dict__
+
+@trace_span("service: get_payment_transaction_by_id")
+def get_payment_transaction_by_id(session: Session, payment_transaction_id: str) -> PaymentTransactionResponseModel:
+    payment_transaction = session.query(PaymentTransaction).filter(PaymentTransaction.id == payment_transaction_id).first()
+    if not payment_transaction:
+        raise NotFound(f"payment_transaction with id {payment_transaction_id} not found")
+    return payment_transaction.__dict__
+
+@trace_span("service: delete_payment_transaction")
+def delete_payment_transaction(session: Session, payment_transaction_id: str):
+    payment_transaction = session.query(PaymentTransaction).filter(PaymentTransaction.id == payment_transaction_id).first()
+    if not payment_transaction:
+        raise NotFound(f"payment_transaction with id {payment_transaction_id} not found")
+    session.delete(payment_transaction)
+    session.commit()
+    return True
+
+@trace_span("service: search_payment_transactions")
+def search_payment_transactions(session: Session, filter) -> PaymentTransactionSearchResults:
+
+    query = session.query(PaymentTransaction)
+    if filter.DisplayCode :
+        query = query.filter(PaymentTransaction.DisplayCode == filter.DisplayCode)
+    if filter.InvoiceNumber :
+        query = query.filter(PaymentTransaction.InvoiceNumber == filter.InvoiceNumber)
+    if filter.BankTransactionId:
+        query = query.filter(PaymentTransaction.BankTransactionId == filter.BankTransactionId)
+    if filter.InitiatedDate:
+        query = query.filter(PaymentTransaction.InitiatedDate == filter.InitiatedDate)
+    if filter.CustomerId:
+        query = query.filter(PaymentTransaction.CustomerId == filter.CustomerId)
+    if filter.OrderId:
+        query = query.filter(PaymentTransaction.OrderId == filter.OrderId)
+    if filter.PaymentMode:
+        query = query.filter(PaymentTransaction.PaymentMode == filter.PaymentMode)
+    if filter.PaymentAmount:
+        query = query.filter(PaymentTransaction.PaymentAmount == filter.PaymentAmount)
+    payment_transactions = query.all()
+
+    results = PaymentTransactionSearchResults(
+        TotalCount=len(payment_transactions),
+        ItemsPerPage=filter.ItemsPerPage,
+        PageNumber=filter.PageNumber,
+        OrderBy=filter.OrderBy,
+        OrderByDescending=filter.OrderByDescending,
+        Items=payment_transactions
+    )
+
+
