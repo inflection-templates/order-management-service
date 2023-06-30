eb08be4348296e7c2e9de795516e45fc29eb42aa
Priyanka-Inflectionzone <priyanka.kale@inflectionzone.com>
2023-06-30 15:21:21 +0530

    Modified routes

--- app/api/merchant/merchant_routes.py
+++ app/api/merchant/merchant_routes.py
@@ -20,23 +20,23 @@ router = APIRouter(
 )
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=ResponseModel[MerchantResponseModel|None])
-async def create_customer(model: MerchantCreateModel, db_session = Depends(get_db_session)):
+async def create_merchant(model: MerchantCreateModel, db_session = Depends(get_db_session)):
     return  create_merchant_(model, db_session)
 
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=ResponseModel[MerchantResponseModel|None])
-async def get_customer_by_id(id: str, db_session = Depends(get_db_session)):
+async def get_merchant_by_id(id: str, db_session = Depends(get_db_session)):
     return get_merchant_by_id_(id, db_session)
 
 @router.put("/{id}", status_code=status.HTTP_200_OK, response_model=ResponseModel[MerchantResponseModel|None])
-async def update_customer(id: str, model: MerchantUpdateModel, db_session = Depends(get_db_session)):
+async def update_merchant(id: str, model: MerchantUpdateModel, db_session = Depends(get_db_session)):
     return update_merchant_(id, model, db_session)
 
 @router.delete("/{id}", status_code=status.HTTP_200_OK, response_model=ResponseModel[bool])
-async def delete_customer(id: str, db_session = Depends(get_db_session)):
+async def delete_merchant(id: str, db_session = Depends(get_db_session)):
     return delete_merchant_(id, db_session)
 
 @router.get("/search", status_code=status.HTTP_200_OK, response_model=ResponseModel[MerchantSearchResults|None])
-async def search_customer(
+async def search_merchants(
         query_params: MerchantSearchFilter = Depends(),
         db_session = Depends(get_db_session)):
     filter = MerchantSearchFilter(**query_params.dict())
