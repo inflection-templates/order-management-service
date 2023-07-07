9c57878a27f9be2c46a4ee36947b1c98c58ac1d1
Priyanka-Inflectionzone <priyanka.kale@inflectionzone.com>
2023-07-07 10:04:10 +0530

    Modified test

--- tests/customer/test_customer_routes.py
+++ tests/customer/test_customer_routes.py
@@ -4,10 +4,10 @@ import json
 
 def test_create_customer(test_client):
     create_model = get_customer_create_model()
-    json_create_model = json.dumps(create_model, default=str) 
+    json_create_model = json.dumps(create_model, default=str)
     json_ = json.loads(json_create_model)
-    response = test_client.post("/api/v1/customers/", 
-                                headers = { "Content-Type": "application/json"}, 
+    response = test_client.post("/api/v1/customers/",
+                                headers = { "Content-Type": "application/json"},
                                 json=json_)
     response_body = response.json()
     assert response.status_code == 201
