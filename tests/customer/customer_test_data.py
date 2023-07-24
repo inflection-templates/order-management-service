dc62f34c28a07989135edb2685da70c247445c60
Priyanka-Inflectionzone <priyanka.kale@inflectionzone.com>
2023-07-24 16:33:17 +0530

    Added unit tests for all routes of customer model

--- tests/customer/customer_test_data.py
+++ tests/customer/customer_test_data.py
@@ -4,14 +4,23 @@ import uuid
 
 from app.domain_types.schemas.customer import CustomerCreateModel
 
-
 def get_customer_create_model():
     fake = Faker()
     model = {
         "ReferenceId": uuid.uuid4(),
         "Name": fake.name(),
         "Email": fake.email(),
-        "Phone": fake.phone_number().replace("-", ""),
+        "Phone": fake.phone_number().replace("-", "")[:12],
+    }
+    print(model)
+    return model
+
+def get_customer_update_model():
+    fake = Faker()
+    model = {
+        "Name": fake.name(),
+        "Email": fake.email(),
+        "Phone": fake.phone_number().replace("-", "")[:12],
     }
     print(model)
     return model
--- tests/customer/test_customer_routes.py
+++ tests/customer/test_customer_routes.py
@@ -1,4 +1,4 @@
-from tests.customer.customer_test_data import get_customer_create_model
+from tests.customer.customer_test_data import get_customer_create_model, get_customer_update_model
 from tests.test_config import test_client
 import json
 
@@ -10,6 +10,31 @@ def test_create_customer(test_client):
                                 headers = { "Content-Type": "application/json"},
                                 json=json_)
     response_body = response.json()
+    print(response_body)
     assert response.status_code == 201
+    assert "id" in response_body["Data"]
+    customer_id = response_body["Data"]["id"]
+    return customer_id
 
+def test_get_customer_by_id(test_client):
+    customer_id = test_create_customer(test_client)
+    response = test_client.get(f"/api/v1/customers/{customer_id}")
+    response_body = response.json()
+    assert response.status_code == 200
 
+def test_update_customer(test_client):
+    update_model = get_customer_update_model()
+    customer_id = test_create_customer(test_client)
+    json_update_model = json.dumps(update_model, default=str)
+    json_ = json.loads(json_update_model)
+    response = test_client.put(f"/api/v1/customers/{customer_id}",
+                               headers = { "Content-Type": "application/json"},
+                                json=json_)
+    response_body = response.json()
+    assert response.status_code == 200
+
+def test_delete_customer(test_client):
+    customer_id = test_create_customer(test_client)
+    response = test_client.delete(f"/api/v1/customers/{customer_id}")
+    response_body = response.json()
+    assert response.status_code == 200
